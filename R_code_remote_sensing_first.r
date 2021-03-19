4c15e65a5ca65f3d057655d6e526280c859e5b60
annacandotti <80513538+annacandotti@users.noreply.github.com>
2021-03-19 10:50:30 +0100

    Update R_code_remote_sensing_first.r

--- R_code_remote_sensing_first.r
+++ R_code_remote_sensing_first.r
@@ -5,6 +5,7 @@ library(raster) # per richiamare il pacchetto raster installato dall´esterno
 p224r63_2011<-brick("p224r63_2011_masked.grd") #importa immagine e associa nome
 p224r63_2011 #immagine info
 plot(p224r63_2011) #per visualizzare le Rif nelle singole bande
+#B1 blu #B2 verde #B3 rosso #B4 nir #B5 swir #B6 therm infr #B7 swir #B8 pancromatico
 cl <- colorRampPalette(c("black","grey","light grey")) (100) #per impostare i colori della scala di Rif
 plot(p224r63_2011, col=cl) #per associare la palette all´immagine #argomenti di una funzione sempre separati da una virgola
 cl <- colorRampPalette(c("purple","red","orange", "yellow")) (100) #altri colori

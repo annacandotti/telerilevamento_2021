c21b96619efa084572e5f6814111f5bfa18be76f
annacandotti <80513538+annacandotti@users.noreply.github.com>
2021-04-17 16:59:21 +0200

    Update R_code_knitr.r

--- R_code_knitr.r
+++ R_code_knitr.r
@@ -1,1 +1,4 @@
 #R_code_knitr.r
+setwd("C:/lab/")
+require(knitr)
+stitch("R_code_greenland.r", template=system.file("misc", "knitr-template.Rnw", package="knitr")) #per generare un report con grafici che viene salvato nella cartella da cui prende il codice

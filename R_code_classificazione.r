0286dd64dc9e099594525d7dd82b2c2cff4ea8cc
annacandotti <80513538+annacandotti@users.noreply.github.com>
2021-04-23 10:01:00 +0200

    Update and rename solar_orbiter_classificazione_R.r to R_code_classificazione.r

--- R_code_classificazione.r
+++ R_code_classificazione.r
@@ -7,6 +7,28 @@ plotRGB(so, 1,2,3, stretch="lin") #visualizzazione RGB levels 3 livelli energeti
 soc <- unsuperClass(so, nClasses=3) #classificazione non supervisionata con 3 classi
 plot(soc$map)
 set.seed(42) #per fare in modo che la classificazione venga elaborata sempre allo stesso modo, stesso numero random di training sites
+cl <- colorRampPalette(c("yellow", "black", "red"))(100)
+plot(soc20$map, col=cl)
+
+#Grand Canyon
+library(raster)
+library(RStoolbox)
+setwd("C:/lab")
+
+gc <- brick("dolansprings_oli_2013088_canyon_lrg.jpg")
+gc
+plotRGB(gc, r=1, g=2, b=3, stretch="lin")
+plotRGB(gc, r=1, g=2, b=3, stretch="hist")
+
+gcc2 <- unsuperClass(gc, nClasses=2)
+gcc2
+plot(gcc2$map)
+gcc4 <- unsuperClass(gc, nClasses=4)
+gcc4
+plot(gcc4$map)
+
+
+
 
 
 

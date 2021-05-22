e4ed96a4c7e6170472a8e1a17172bba5d6238030
annacandotti <80513538+annacandotti@users.noreply.github.com>
2021-05-22 12:48:32 +0200

    Update R_code_variability.r

--- R_code_variability.r
+++ R_code_variability.r
@@ -52,6 +52,58 @@ plot(ndvisd25, col = clsd)
 sentpca <- rasterPCA(sent)
 plot(sentpca$map)
 summary(sentpca$model)
+#la prima pc descrive il 67% della variabilita
+
+
+#deviazione standard della  prima cp
+pc1sd5 <- focal(pc1, w=matrix(1/25, nrow=5, ncol=5), fun=sd)
+clsd <- colorRampPalette(c("blue", "green", "pink", "magenta", "orange", "brown")) (100)
+plot(pc1sd5, col=clsd)
+                         
+
+source("source_ggplot.r") #carica direttamente il codice se presente nella wd
+
+#plottare i dati tramite ggplot
+#definizione della geometria del grafico, a punti con geom_point, a linee con geom_line, con i raster geom_raster creo la mappa
+#mappo x,y e il valore (layer) della dev st
+#per individuare le discontinuita, variabilita geomorfologica o ecologica, zone omogenee = meno diversita
+ggplot() +
+geom_raster(pc1sd5, mapping = aes (x = x, y = y, fill=layer))
+
+#funzione scale_fill_viridis() dentro al pacchetto viridis per applicare una legenda senza dover scrivere la colorRampPalette
+p1 <- ggplot() +
+geom_raster(pc1sd5, mapping = aes (x = x, y = y, fill=layer)) +
+scale_fill_viridis() +
+ggtitle("Standard deviation of PC1 by viridis colour scale") #aggiungo titolo al plot con legenda
+
+p2 <- ggplot() +
+geom_raster(pc1sd5, mapping = aes (x = x, y = y, fill=layer)) +
+scale_fill_viridis(option = "magma") +
+ggtitle("Standard deviation of PC1 by magma colour scale")
+#visualizzo le zone con alta dev st
+
+ggplot() +
+geom_raster(pc1sd5, mapping = aes (x = x, y = y, fill=layer)) +
+scale_fill_viridis(option = "inferno") +
+ggtitle("Standard deviation of PC1 by inferno colour scale")
+
+p3 <- ggplot() +
+geom_raster(pc1sd5, mapping = aes (x = x, y = y, fill=layer)) +
+scale_fill_viridis(option = "turbo") +
+ggtitle("Standard deviation of PC1 by turbo colour scale")
+
+#funzione per unire piu mappe virids(p1), magma(p2), turbo(p3) con grid.arrange della libreria grid.Extra
+grid.arrange(p1, p2, p3, nrow= 3)
+grid.arrange(p1, p2, p3, nrow= 1)
+
+
+
+
+
+
+
+
+
 
 
 
